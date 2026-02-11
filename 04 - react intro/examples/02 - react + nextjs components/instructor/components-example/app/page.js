de4fb95c467456d537e6f79b30625508651400c3
Oliver Antoniu <oliver+github@abolish.golf>
2026-02-11 11:43:26 -0700

    add [04 - react]: ex02 - simple first component

--- 04 - react intro/examples/02 - react + nextjs components/instructor/components-example/app/page.js
+++ 04 - react intro/examples/02 - react + nextjs components/instructor/components-example/app/page.js
@@ -1,11 +1,12 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import Hello from "./components/Hello.js";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        
+        <h1>My React App hehe</h1>
+        <Hello />
       </main>
     </div>
   );
